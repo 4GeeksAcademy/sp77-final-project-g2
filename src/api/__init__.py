e9ebb2c3128bf1222735f82a3fefcde41974d09b
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-10-12 11:06:34 +0000

    advisor complete

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from .user import user_bp
+from .ideas import ideas_bp
 
 def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(user_bp)
+    app.register_blueprint(ideas_bp)
 
     return app
--- src/api/ideas/ideas.py
+++ src/api/ideas/ideas.py
@@ -8,37 +8,37 @@ from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from api.models import db, Users, Ideas, FavoriteIdeas
 from openai import OpenAI
-import requests
 
 @ideas_bp.route('/advisor', methods=['POST'])
 def advisor():
     response_body = {}
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        organization=os.getenv("OPENAI_ORGANIZATION"),
+        project=os.getenv("OPENAI_PROJECT")
+    )
 
     # budget = request.json.get('budget')
     # country = request.json.get('country')
     # area = request.json.get('area')
 
-    response = openai.ChatCompletion.create(
+    user_message = request.json.get('message')
+
+    response = client.chat.completions.create(
         model = "gpt-4o",
         messages = [
             {"role": "system",
-            "content": [
-                {"type": "text",
-                "text": "You are a business consultant who generates one business idea. The user will give you their budget, their country and the sector where they would like to start."}
-            ]}
+            "content": "You are a business consultant who generates one business idea. The user will give you their budget, their country and the sector where they would like to start."},
+            {"role": "user", "content": user_message}
         ],
         temperature = 1,
         max_completion_tokens = 250,
-        n = 5,
+        n = 1,
         frequency_penalty=0,
         presence_penalty=0
     )
 
-    ideas = [choice['message']['content'].strip() for choice in response['choices']]
+    ideas = response.choices[0].message.content.strip()
     response_body['message'] = f"Ideas generadas para ti"
     response_body['ideas'] = ideas
     return response_body, 200
-
-
-    
