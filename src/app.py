2eb97ff569a471466716b8ff71ed3d4152e1e2cd
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-10-12 17:18:11 +0200

    Ideas (#6)

--- src/app.py
+++ src/app.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from api.utils import APIException, generate_sitemap
 from api.models import db
-from api import user_bp
+from api import create_app
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
@@ -15,7 +15,7 @@ from flask_jwt_extended import JWTManager
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
-app = Flask(__name__)
+app = create_app()
 app.url_map.strict_slashes = False
 # Database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -29,8 +29,8 @@ db.init_app(app)
 # Others configutations
 setup_admin(app)  # Add the admin
 setup_commands(app)  # Add the admin
-app.register_blueprint(user_bp, url_prefix='/api')
 # app.register_blueprint(user_bp, url_prefix='/api')
+# app.register_blueprint(ideas_bp, url_prefix='/api')
 # app.register_blueprint(user_bp, url_prefix='/api')
 # app.register_blueprint(user_bp, url_prefix='/api')
 #  Setup the Flask-JWT.Extended extension
