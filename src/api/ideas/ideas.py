406519f2741513a052d83c23ca901bb1a18841fa
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-10-12 08:13:41 +0000

    endpoint AI

--- src/api/ideas/ideas.py
+++ src/api/ideas/ideas.py
@@ -1,7 +1,8 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from . import user_bp
+import os
+from . import ideas_bp
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
@@ -12,4 +13,32 @@ import requests
 @ideas_bp.route('/advisor', methods=['POST'])
 def advisor():
     response_body = {}
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    # budget = request.json.get('budget')
+    # country = request.json.get('country')
+    # area = request.json.get('area')
+
+    response = openai.ChatCompletion.create(
+        model = "gpt-4o",
+        messages = [
+            {"role": "system",
+            "content": [
+                {"type": "text",
+                "text": "You are a business consultant who generates one business idea. The user will give you their budget, their country and the sector where they would like to start."}
+            ]}
+        ],
+        temperature = 1,
+        max_completion_tokens = 250,
+        n = 5,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    ideas = [choice['message']['content'].strip() for choice in response['choices']]
+    response_body['message'] = f"Ideas generadas para ti"
+    response_body['ideas'] = ideas
+    return response_body, 200
+
+
     
