4c2c21d68289f387d0029e187a03a4ad9ee49c28
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-10-22 13:07:39 +0000

    converter complete front

--- src/front/js/pages/Converter.jsx
+++ src/front/js/pages/Converter.jsx
@@ -4,6 +4,7 @@ import Flag from 'react-world-flags';
 
 const Converter = () => {
     const { store, actions } = useContext(Context);
+
     const [fromCurrency, setFromCurrency] = useState('USD');
     const [toCurrency, setToCurrency] = useState('EUR');
     const [amount, setAmount] = useState(0);
@@ -18,37 +19,25 @@ const Converter = () => {
         { code: 'INR', country: 'IN' }, 
         { code: 'MXN', country: 'MX' }
     ];
-
+    const filteredToCurrencyOptions = currencyOptions.filter(currency => currency.code !== fromCurrency);
+    
     const handleConvert = () => {
         actions.getConvert(fromCurrency, toCurrency, amount);
     };
 
-    const filteredToCurrencyOptions = currencyOptions.filter(currency => currency.code !== fromCurrency);
-
     return (
         <div className="converter-container">
             <h1>Convertir Moneda</h1>
-
             <div className="input-container">
                 <label htmlFor="amount">Importe:</label>
-                <input 
-                    type="number" 
-                    id="amount" 
-                    value={amount} 
-                    onChange={(e) => setAmount(e.target.value)} 
-                    placeholder="Cantidad"
-                />
+                <input type="number" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Cantidad" required/>
             </div>
 
             <div className="currency-selector">
                 <label htmlFor="fromCurrency">De:</label>
                 <div className="currency-option">
                     <Flag code={currencyOptions.find(c => c.code === fromCurrency).country} height="24" />  
-                    <select
-                        id="fromCurrency"
-                        value={fromCurrency}
-                        onChange={(e) => setFromCurrency(e.target.value)}
-                    >
+                    <select id="fromCurrency" value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
                         {currencyOptions.map((currency, index) => (
                             <option key={index} value={currency.code}>
                                 {currency.code}
@@ -60,11 +49,7 @@ const Converter = () => {
                 <label htmlFor="toCurrency">A:</label>
                 <div className="currency-option">
                     <Flag code={currencyOptions.find(c => c.code === toCurrency).country} height="24" />
-                    <select
-                        id="toCurrency"
-                        value={toCurrency}
-                        onChange={(e) => setToCurrency(e.target.value)}
-                    >
+                    <select id="toCurrency" value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
                         {filteredToCurrencyOptions.map((currency, index) => (
                             <option key={index} value={currency.code}>
                                 {currency.code}
@@ -73,9 +58,9 @@ const Converter = () => {
                     </select>
                 </div>
             </div>
-
             <button onClick={handleConvert}>Convertir</button>
 
+            {/* Mostrar Resultado de Conversión */}
             {store.convertedAmount !== 0 && (
                 <div>
                     <p>Tasa de conversión: {store.conversionRate}</p>
@@ -84,6 +69,6 @@ const Converter = () => {
             )}
         </div>
     );
-};
+}
 
 export default Converter;
