b3fde1dc946cdf14f32da57ea8e8a590bf2ab781
Joan Figueira <joanfigueira365@gmail.com>
2024-10-28 21:29:40 +0100

    Converter styles (#38)
    * feat: Models
    
    * WIP converter styles

--- src/front/js/component/Body.jsx
+++ src/front/js/component/Body.jsx
@@ -51,7 +51,7 @@ export const Body = () => {
                             <h5>Do you have a project in mind?</h5>
                             <p>We will help you with our AI chat</p>
                             <Link to="/Login">
-                                <button type="button" className="btn btn-info btn-lg">Log In</button>
+                                <button type="button" className="button-modern">Log In</button>
                             </Link>
                         </div>
                     </div>
@@ -61,7 +61,7 @@ export const Body = () => {
                             <h5>AI will give you the best ideas</h5>
                             <p>Choose a budget for your project, we will give you several ideas for your business</p>
                             <Link to="/Login">
-                                <button type="button" className="btn btn-info btn-lg">Log In</button>
+                                <button type="button" className="button-modern">Log In</button>
                             </Link>
                         </div>
                     </div>
@@ -71,7 +71,7 @@ export const Body = () => {
                             <h5>The power of AI in your hands</h5>
                             <p>With AI and your answers we will customize the project that best suits you</p>
                             <Link to="/Login">
-                                <button type="button" className="btn btn-info btn-lg">Log In</button>
+                                <button type="button" className="button-modern">Log In</button>
                             </Link>
                         </div>
                     </div>
@@ -99,21 +99,21 @@ export const Body = () => {
                         <span id="info-img" className="mb-0"><i className="fa-solid fa-users"></i></span>
                         <h2 className="fw-normal text-center mt-0">About Us</h2>
                         <p>At our team, we believe that the best business ideas can come at any time, and we are here to help you bring them to life.</p>
-                        <p><a className="btn btn-info btn-lg" href="#">View details »</a></p>
+                        <p><a className="button-modern" href="#">View details »</a></p>
                     </div>
 
                     <div className="col-lg-4">
                         <span id="info-img" className="mb-0"><i className="fa-solid fa-building"></i></span>
                         <h2 className="fw-normal text-center mt-0">Service</h2>
                         <p>We offer a variety of services tailored to specific project needs. Through our chat you can have ideas for ventures immediately.</p>
-                        <p><a className="btn btn-info btn-lg" href="#">View details »</a></p>
+                        <p><a className="button-modern" href="#">View details »</a></p>
                     </div>
 
                     <div className="col-lg-4">
                         <span id="info-img" className="mb-0"><i className="fa-solid fa-address-book"></i></span>
                         <h2 className="fw-normal text-center mt-0">Contact</h2>
                         <p>Ready to take your project to the next level? Sign up and start using AI for free to get project ideas.</p>
-                        <p><a className="btn btn-info btn-lg" href="#">View details »</a></p>
+                        <p><a className="button-modern" href="#">View details »</a></p>
                     </div>
 
                 </div>
@@ -201,7 +201,7 @@ export const Body = () => {
                         <h4 className="featurette-heading fw-normal">Stay Informed with Our Relevant News</h4>
                         <p className="lead">In this section we bring you the latest news related to the world of artificial intelligence, technological innovation and the impact they have on the creation of business projects. Our team of experts makes sure to provide you with the latest and most valuable information so that you are always aware of the trends that can transform your ideas into successes.</p>
                         <Link to="/news">
-                            <button type="button" className="btn btn-info btn-lg">News</button>
+                            <button type="button" className="button-modern">News</button>
                         </Link>
                     </div>
                     <div className="col-md-5">
@@ -219,7 +219,7 @@ export const Body = () => {
                         <h4 className="featurette-heading fw-normal lh-1">Ready to get started?</h4>
                         <p>Talking to our AI is easy! Start a conversation now and find out how we can help you take your project to the next level.</p>
                         <Link to="/advisor">
-                            <button type="button" className="btn btn-info btn-lg">Advisor</button>
+                            <button type="button" className="button-modern">Advisor</button>
                         </Link>
                     </div>
                     <div className="col-md-5">
@@ -236,7 +236,7 @@ export const Body = () => {
                         <h4 className="featurette-heading fw-normal lh-1">Convert your Currencies Now</h4>
                         <p>Start using our currency converter and get the most accurate and up-to-date exchange rates for all your international financial needs!</p>
                         <Link to="/converter">
-                            <button type="button" className="btn btn-info btn-lg">Converter</button>
+                            <button type="button" className="button-modern">Converter</button>
                         </Link>
                     </div>
                     <div className="col-md-5 order-md-1">
