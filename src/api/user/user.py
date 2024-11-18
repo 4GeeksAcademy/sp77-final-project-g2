cef7f586dfd0a9d50c9cae2b3ecd7802e8a608f3
Ricard Mesegué <ricardm1995@gmail.com>
2024-11-18 20:10:46 +0000

    feat: delete button user

--- src/api/user/user.py
+++ src/api/user/user.py
@@ -32,6 +32,20 @@ def users():
     response_body['results'] = results
     return response_body, 200
 
+@user_bp.route('/admin/users/<int:user_id>', methods=['DELETE'])
+@jwt_required()
+def delete_user(user_id):
+    response_body = {}
+    user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+    if not user:
+        response_body['message'] = "Usuario no encontrado"
+        return response_body, 404
+    db.session.delete(user)
+    db.session.commit()
+
+    response_body['message'] = "Usuario eliminado con éxito"
+    return response_body, 200
+
 
 @user_bp.route('/login', methods=['POST'])
 def login():
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -5,16 +5,16 @@ const getState = ({ getStore, getActions, setStore }) => {
 			favoriteIdeas: [],
 			news: [],
 			fromCurrency: "",
-            toCurrency: "",
-            amount: 0,
-            conversionRate: 0,
-            convertedAmount: 0,
+			toCurrency: "",
+			amount: 0,
+			conversionRate: 0,
+			convertedAmount: 0,
 
 			user: {},
 			isLoged: false,
 			isPremium: false,
-			alert: { 
-				message: "", 
+			alert: {
+				message: "",
 				type: ""
 			},
 			clientSecret: null,
@@ -26,6 +26,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				setStore({ alert: { message, type } });
 				setTimeout(() => setStore({ alert: { message: "", type: "" } }), 2000);
 			},
+
 			getIdeas: async (budget, country, area) => {
 				const uri = `${process.env.BACKEND_URL}/advisor`;
 				const options = {
@@ -47,6 +48,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const data = await response.json();
 				setStore({ ideas: data.ideas });
 			},
+
 			getNews: async (category) => {
 				const uri = `${process.env.BACKEND_URL}/news?category=${category}`;
 				const options = {
@@ -60,6 +62,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const data = await response.json()
 				setStore({ news: data.news });
 			},
+
 			logIn: async (dataToSend) => {
 				const uri = `${process.env.BACKEND_URL}/login`;
 				const options = {
@@ -78,16 +81,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const data = await response.json();
 				localStorage.setItem('token', data.access_token);
 				localStorage.setItem('user', JSON.stringify(data.results));
-				setStore({isLoged: true, user: data.results.email, isPremium: data.results.is_premium});
+				setStore({ isLoged: true, user: data.results.email, isPremium: data.results.is_premium });
 				getActions().showAlert("Bienvenido de nuevo!", "success");
 				return true;
 			},
+
 			logOut: () => {
-				setStore({isLoged: false, user: {}});
+				setStore({ isLoged: false, user: {} });
 				localStorage.removeItem('token');
 				localStorage.removeItem('user');
 				getActions().showAlert("Hasta pronto!", "success");
 			},
+
 			isLogged: () => {
 				const token = localStorage.getItem('token');
 				if (token) {
@@ -98,6 +103,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Usuario no autenticado");
 				}
 			},
+
 			signUp: async (dataToSend) => {
 				const uri = `${process.env.BACKEND_URL}/signup`;
 				const options = {
@@ -108,7 +114,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					body: JSON.stringify(dataToSend)
 				};
 				const response = await fetch(uri, options);
-				if(!response.ok){
+				if (!response.ok) {
 					console.log(response.status);
 					return false;
 				}
@@ -121,6 +127,38 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().showAlert("Registro exitoso. Bienvenido/a!", "success");
 				return true;
 			},
+
+			deleteUser: async (userId) => {
+				if (!userId || userId === 'undefined') {
+					console.error("User ID is undefined or invalid");
+					return false;
+				}
+			
+				try {
+					const token = localStorage.getItem('token');
+					if (!token) {
+						console.error("Token no encontrado. El usuario no está autenticado.");
+						return false;
+					}
+			
+					const response = await fetch(`${process.env.BACKEND_URL}/admin/users/${userId}`, {
+						method: 'DELETE',
+						headers: {
+							'Content-Type': 'application/json',
+							'Authorization': `Bearer ${token}`,
+						},
+					});
+			
+					if (!response.ok) throw new Error("Error al eliminar el usuario");
+					const data = await response.json();
+					console.log(data.message);
+					return true;
+				} catch (error) {
+					console.error("Error al eliminar el usuario:", error);
+					return false;
+				}
+			},
+
 			resetPassword: async (token, newPassword) => {
 				const uri = `${process.env.BACKEND_URL}/reset-password`;
 				const options = {
@@ -133,12 +171,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 				};
 				const response = await fetch(uri, options);
 				const data = await response.json();
-				
+
 				if (response.ok) {
 					getActions().showAlert("Contraseña actualizada con éxito", "success");
 				}
 				return { success: response.ok, message: data.message };
 			},
+
 			requestPasswordReset: async (email) => {
 				const uri = `${process.env.BACKEND_URL}/request-password-reset`;
 				const options = {
@@ -150,30 +189,34 @@ const getState = ({ getStore, getActions, setStore }) => {
 				};
 				const response = await fetch(uri, options);
 				const data = await response.json();
-				
+
 				if (response.ok) {
 					getActions().showAlert("Revisa tu correo para el enlace de restablecimiento", "success");
 				}
 				return { success: response.ok, message: data.message };
 			},
+
 			getConvert: async (fromCurrency, toCurrency, amount) => {
 				const uri = `${process.env.BACKEND_URL}/converter?from_currency=${fromCurrency}&to_currency=${toCurrency}&amount=${amount}`;
 				const options = {
 					method: 'GET'
 				};
 				const response = await fetch(uri, options);
-				if(!response.ok) {
+				if (!response.ok) {
 					console.log(response.status);
 					return;
 				}
 				const data = await response.json();
 				console.log(data.results);
-				setStore({fromCurrency: data.results.base_code,
-						 toCurrency: data.results.target_code,
-						 originalAmount: amount,
-						 conversionRate: data.results.conversion_rate,
-						 convertedAmount: data.results.conversion_result})
+				setStore({
+					fromCurrency: data.results.base_code,
+					toCurrency: data.results.target_code,
+					originalAmount: amount,
+					conversionRate: data.results.conversion_rate,
+					convertedAmount: data.results.conversion_result
+				})
 			},
+
 			accessProtected: async () => {
 				const uri = `${process.env.BACKEND_URL}/protected`;
 				const token = localStorage.getItem('token');
@@ -185,12 +228,13 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}
 				};
 				const response = await fetch(uri, options);
-				if(!response.ok){
+				if (!response.ok) {
 					console.log(response.status);
 					return;
 				}
 				const data = await response.json()
 			},
+
 			addFavoriteIdea: async (newFavorite) => {
 				const token = localStorage.getItem('token');
 				if (!token) {
@@ -198,7 +242,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					getActions().showAlert("Por favor, inicia sesión para agregar ideas a favoritos.", "warning");
 					return;
 				}
-			
+
 				const uri = `${process.env.BACKEND_URL}/favorite-ideas`;
 				const options = {
 					method: 'POST',
@@ -208,7 +252,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					},
 					body: JSON.stringify(newFavorite)
 				};
-			
+
 				try {
 					const response = await fetch(uri, options);
 					if (!response.ok) {
@@ -217,10 +261,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 						getActions().showAlert("Error al guardar la idea favorita: " + (errorData.message || "Inténtalo nuevamente"), "danger");
 						return;
 					}
-			
+
 					const data = await response.json();
 					console.log("Respuesta del backend:", data);
-			
+
 					if (data.favoriteIdea) {
 						setStore({ favoriteIdeas: [...getStore().favoriteIdeas, data.favoriteIdea] });
 						getActions().showAlert("Nueva Idea guardada!", "success");
@@ -232,7 +276,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.error("Error de red o de servidor:", error);
 					getActions().showAlert("Error de red. Inténtalo nuevamente.", "danger");
 				}
-			},			
+			},
+
 			removeFavoriteIdea: async (ideaId) => {
 				const uri = `${process.env.BACKEND_URL}/favorite-ideas/${ideaId}`;
 				const token = localStorage.getItem('token');
@@ -243,7 +288,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						'Authorization': `Bearer ${token}`
 					}
 				};
-				
+
 				const response = await fetch(uri, options);
 				if (!response.ok) {
 					console.error("Error al eliminar la idea favorita:", response.status);
@@ -252,7 +297,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				setStore({ favoriteIdeas: getStore().favoriteIdeas.filter(idea => idea.id !== ideaId) });
 				getActions().showAlert("Idea favorita eliminada!", "success");
-			},			
+			},
+
 			getFavoriteIdeas: async () => {
 				const uri = `${process.env.BACKEND_URL}/favorite-ideas`;
 				const token = localStorage.getItem('token');
@@ -267,19 +313,19 @@ const getState = ({ getStore, getActions, setStore }) => {
 						'Authorization': `Bearer ${token}`
 					}
 				};
-			
+
 				try {
 					const response = await fetch(uri, options);
 					console.log("Status:", response.status);
-			
+
 					if (!response.ok) {
 						console.error("Error al obtener ideas favoritas:", response.status);
 						return;
 					}
-			
+
 					const data = await response.json();
 					console.log("Datos de ideas favoritas:", data.results);
-					
+
 					if (Array.isArray(data.results)) {
 						setStore({ favoriteIdeas: data.results });
 						console.log("favoriteIdeas actualizado en el estado:", getStore().favoriteIdeas);
@@ -291,6 +337,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.error("Error al obtener las ideas favoritas:", error);
 				}
 			},
+
 			getIdeaTips: async (ideaId) => {
 				if (!getStore().isPremium) {
 					getActions().showAlert("Necesitas ser un usuario premium para acceder a esta función.", "warning");
@@ -299,7 +346,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				const uri = `${process.env.BACKEND_URL}/favorite-ideas/${ideaId}/tips`;
 				const token = localStorage.getItem('token');
-			
+
 				const options = {
 					method: 'GET',
 					headers: {
@@ -307,7 +354,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						'Authorization': `Bearer ${token}`
 					}
 				};
-			
+
 				try {
 					const response = await fetch(uri, options);
 					if (!response.ok) {
@@ -316,7 +363,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						setStore({ tips: [] });
 						return null;
 					}
-			
+
 					const data = await response.json();
 					setStore({ tips: data.tips });
 					return data.tips;
@@ -326,7 +373,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					setStore({ tips: [] });
 					return null;
 				}
-			},								
+			},
+
 			startCheckoutSession: async () => {
 				const uri = `${process.env.BACKEND_URL}/create-checkout-session`;
 				const options = {
@@ -343,13 +391,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const data = await response.json();
 				window.location.href = data.url;
 			},
+
 			checkPremiumStatus: async () => {
 				const token = localStorage.getItem('token');
 				if (!token) {
 					console.error("Token no encontrado. El usuario no está autenticado.");
 					return;
 				}
-			
+
 				const uri = `${process.env.BACKEND_URL}/user-status`;
 				const options = {
 					method: 'GET',
@@ -358,21 +407,21 @@ const getState = ({ getStore, getActions, setStore }) => {
 						'Authorization': `Bearer ${token}`
 					}
 				};
-			
+
 				try {
 					const response = await fetch(uri, options);
 					if (!response.ok) {
 						console.error("Error al verificar el estado de premium:", response.status);
 						return;
 					}
-			
+
 					const data = await response.json();
 					console.log("Estado de usuario actualizado:", data);
 					setStore({ isPremium: data.is_premium });
 				} catch (error) {
 					console.error("Error de red o de servidor al verificar el estado de premium:", error);
 				}
-			}			
+			}
 		}
 	};
 };
